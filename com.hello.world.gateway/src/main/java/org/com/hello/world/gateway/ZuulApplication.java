6a2201b57d2c69d1132f6cc6ee0a4ca1cefafe51
hutao1187 <838533527@qq.com>
2019-05-28 00:56:15 +0800

    zuul网关

---
+++ com.hello.world.gateway/src/main/java/org/com/hello/world/gateway/ZuulApplication.java
@@ -0,0 +1,28 @@
+package org.com.hello.world.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+import lombok.extern.slf4j.Slf4j;
+
+/**
+ * @author hutao
+ *
+ */
+@EnableZuulProxy
+@EnableEurekaClient
+@SpringBootApplication
+@Slf4j
+public class ZuulApplication 
+{
+    public static void main( String[] args )
+    {	
+    	log.info("gateway service start...");
+    	
+        SpringApplication.run(ZuulApplication.class, args);
+        
+        log.info("gateway service started successful...");
+    }
+}
